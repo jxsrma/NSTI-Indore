cc4a9f19c6e2d52737aee1102f114693e302f011
Jash <jashsharma619@gmail.com>
2025-01-06 16:49:51 +0530

    Python Basic

---
+++ HelloPython/kw.py
@@ -0,0 +1,6 @@
+import keyword
+
+kw = keyword.kwlist
+
+
+print(kw)
---
+++ HelloPython/printFunction.py
@@ -0,0 +1,8 @@
+'''This is print function example
+sdfttgggf
+hffh
+tg
+We are learning  python '''
+
+print("hello","world",sep="jash",end="\n")
+print("h","w",sep="*",end="=")
