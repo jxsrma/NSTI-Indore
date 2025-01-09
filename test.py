930522215f13e9c9ad52297114a850d5c9c4e08a
Jash <jashsharma619@gmail.com>
2025-01-09 12:57:38 +0530

    Test

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-for i in range(5):
-    n = int(input(f"Enter number {i}: "))
-    print(n)
+import json
+# student = {
+#     'name': 'Shyam',
+#     'class': '10',
+#     'section': 'B',
+#     'school': 'XYZ'
+# }
+
+# with open('school_data.json', 'w') as jsonFile:
+#     json.dump(student, jsonFile)
+
+# student['marks'] = 67
+
+# with open('school_data.json', 'w') as jsonFile:
+#     json.dump(student, jsonFile)
+
+with open('school_data.json', 'r') as jsonFile:
+    studentdata = json.load(jsonFile)
+    student = studentdata
+
+student['marks'] = student['marks']+1
+
+with open('school_data.json', 'w') as jsonFile:
+    json.dump(student, jsonFile)
