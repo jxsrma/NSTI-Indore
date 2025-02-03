c51af8bb59ee6901e8f428e6f422c3b8b1135d50
Jash <jashsharma619@gmail.com>
2025-02-03 10:20:09 +0530

    Web Crawler Updated

--- WebScraping/requestToWebsites.py
+++ WebScraping/requestToWebsites.py
@@ -1,7 +1,7 @@
 import requests
 
-# url = "https://www.google.com/"
-url = "https://www.microsoft.com/en-in"
+url = "https://www.google.com/"
+# url = "https://www.microsoft.com/en-in"
 # url = "https://quotes.toscrape.com/"
 
 response = requests.get(url)
@@ -9,16 +9,17 @@ print("Response: ", response)
 print()
 print()
 print()
-print("Status: ", response.status_code)
+# print("Status: ", response.status_code)
 print()
 print()
 print()
-print("Headers: ", response.headers)
+# print("Headers: ", response.headers)
 print()
 print()
 print()
 print("Content: ", response.text)
+# print("Content: ", response.content)
 print()
 print()
 print()
-print("Cookies: ", response.cookies)
+print("Cookies: ", response.cookies.keys())
--- test.py
+++ test.py
@@ -1,24 +1,56 @@
-import json
-# student = {
-#     'name': 'Shyam',
-#     'class': '10',
-#     'section': 'B',
-#     'school': 'XYZ'
-# }
+# import requests
+# from bs4 import BeautifulSoup
 
-# with open('school_data.json', 'w') as jsonFile:
-#     json.dump(student, jsonFile)
+# def simple_crawler(url):
+#     # Send a request to the website
+#     response = requests.get(url)
+    
+#     # Check if request was successful
+#     if response.status_code != 200:
+#         print("Failed to retrieve the webpage")
+#         return
+    
+#     # Parse the HTML content
+#     soup = BeautifulSoup(response.text, 'html.parser')
+    
+#     # Extract and print all links on the page
+#     for link in soup.find_all('a'):
+#         href = link.get('href')
+#         if href:
+#             print(href)
 
-# student['marks'] = 67
+# # Example usage
+# simple_crawler('https://edunetfoundation.org/')
 
-# with open('school_data.json', 'w') as jsonFile:
-#     json.dump(student, jsonFile)
 
-with open('school_data.json', 'r') as jsonFile:
-    studentdata = json.load(jsonFile)
-    student = studentdata
+import requests
+from bs4 import BeautifulSoup
 
-student['marks'] = student['marks']+1
+def web_crawler(start_url, max_pages=20): 
+    visited = set()
+    to_visit = [start_url]
+    
+    while to_visit and len(visited) < max_pages:
+        url = to_visit.pop(0)
+        if url in visited:
+            continue
+        
+        print(f"Crawling: {url}")
+        visited.add(url)
+        
+        try:
+            response = requests.get(url)
+            if response.status_code != 200:
+                continue
+        except requests.RequestException:
+            continue
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        for link in soup.find_all('a', href=True):
+            href = link.get('href')
+            if href.startswith('http') and href not in visited:
+                to_visit.append(href)
 
-with open('school_data.json', 'w') as jsonFile:
-    json.dump(student, jsonFile)
+# Example usage
+web_crawler('https://edunetfoundation.org/')
